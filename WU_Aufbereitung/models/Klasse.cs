406c45cf30207e9248b722aee908b7e686668956
FUDlAN <nicole.huabch@web.de>
2020-11-03 07:51:58 +0100

    Sync und puhsne

---
+++ WU_Aufbereitung/models/Klasse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WU_Aufbereitung.models
+{
+    class Klasse
+    {
+        private List<Schueler> schuelerListe = new List<Schueler>();
+        private string bezeichner;
+
+        public Klasse(List<Schueler> schuelerListe, string bezeichner)
+        {
+            this.schuelerListe = schuelerListe;
+            this.bezeichner = bezeichner;
+        }
+
+        #region Getter/Setter
+        public string Bezeichner { get => bezeichner; set => bezeichner = value; }
+        internal List<Schueler> SchuelerListe { get => schuelerListe; set => schuelerListe = value; }
+        #endregion
+    }
+}
