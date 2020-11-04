8d07d898152b737c88063880a90b2444883067e6
Tilman Ludwig Beyer <tilmanludwigbeyer@gmail.com>
2020-11-04 17:02:52 +0100

    Einstellungen hinzugefügt und Startseite dynamisch gemacht

---
+++ WU_Aufbereitung/view/EinstellungenView.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WU_Aufbereitung.view
+{
+    /// <summary>
+    /// Interaktionslogik für EinstellungenView.xaml
+    /// </summary>
+    public partial class EinstellungenView : Window
+    {
+        public EinstellungenView()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAbrechenClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnSpeichernClick(object sender, RoutedEventArgs e)
+        {
+            //TODO Speichern to Prefferences
+            this.Close();
+        }
+    }
+}
