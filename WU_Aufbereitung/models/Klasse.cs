8ae2d7bef4deaa7dd6f8498c0b82f69517c9b61b
FUDlAN <nicole.huabch@web.de>
2020-11-04 17:12:30 +0100

    Ich hab mal was gemacht

--- WU_Aufbereitung/models/Klasse.cs
+++ WU_Aufbereitung/models/Klasse.cs
@@ -8,45 +8,26 @@ namespace WU_Aufbereitung.models
     {
         private List<Schueler> schuelerListe = new List<Schueler>();
         private string bezeichner;
-        private string[] woche;
 
-        public Klasse(List<Schueler> schuelerListe, string bezeichner, string[] woche)
+        public Klasse(List<Schueler> schuelerListe, string bezeichner)
         {
             this.schuelerListe = schuelerListe;
             this.bezeichner = bezeichner;
-            this.woche = woche;
         }
 
         #region Getter/Setter
         public string Bezeichner { get => bezeichner; set => bezeichner = value; }
-        public string[] Woche { get => woche; set => woche = value; }
         internal List<Schueler> SchuelerListe { get => schuelerListe; set => schuelerListe = value; }
 
-        public List<ViewSchueler> GetListViewSchueler()
+        public static List<Schueler> getSampleSchuelerListe()
         {
-            List<ViewSchueler> list = new List<ViewSchueler>();
-            foreach (Schueler s in schuelerListe)
-            {
-                list.Add(new ViewSchueler(s, ));
-            }
-            return null;
-        }
-
-        private 
-
-        public static List<Schueler> getSampleSchuelerListe() {
             List<Schueler> liste = new List<Schueler>();
-            liste.Add(new Schueler("Hubach", "Jean-Pierre", new Fehlzeit[] { new Fehlzeit("02.11", 8, "entschuldigt") }));
-            liste.Add(new Schueler("Beyer", "Tilman", new Fehlzeit[] { new Fehlzeit("03.11", 4, "entschuldigt") }));
-            liste.Add(new Schueler("Schuck", "Christian", new Fehlzeit[] { new Fehlzeit("04.11", 8, "offen") }));
-            liste.Add(new Schueler("Sperling", "Oliver", new Fehlzeit[] { new Fehlzeit("04.11", 3, "beurlaubt") }));
-            liste.Add(new Schueler("Mucke", "Philipp", new Fehlzeit[] { new Fehlzeit("04.11", 2, "entschuldigt") }));
-
+            liste.Add(new Schueler("Hubach", "Jean-Pierre", new Fehlzeit[] { new Fehlzeit("02.11", 8, "entschuldigt") }, 0, 0, 0));
+            liste.Add(new Schueler("Beyer", "Tilman", new Fehlzeit[] { new Fehlzeit("03.11", 4, "entschuldigt") }, 0, 0, 0));
+            liste.Add(new Schueler("Schuck", "Christian", new Fehlzeit[] { new Fehlzeit("04.11", 8, "offen") }, 0, 0, 0));
+            liste.Add(new Schueler("Sperling", "Oliver", new Fehlzeit[] { new Fehlzeit("04.11", 3, "beurlaubt") }, 0, 0, 0));
+            liste.Add(new Schueler("Mucke", "Philipp", new Fehlzeit[] { new Fehlzeit("04.11", 2, "entschuldigt") }, 0, 0, 0));
             return liste;
-        }
-
-        public static string[] GetSampleDatum() { 
-            return new string[]{"01.11", "02.11", "03.11", "04.11", "05.11" };
         }
         #endregion
     }
--- WU_Aufbereitung/models/Verarbeiter.cs
+++ WU_Aufbereitung/models/Verarbeiter.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using Microsoft.Office.Interop;
-using Excel=Microsoft.Office.Interop.Excel;
+using System.Text.RegularExpressions;
+using IronXL;
+using Range = IronXL.Range;
 
 
 namespace WU_Aufbereitung.models
@@ -13,81 +14,76 @@ namespace WU_Aufbereitung.models
     {
         private Schueler[] schuelerListe;
         private int[,] fehlzeitenSummenListe;
+        private string[] daten;
 
         public Verarbeiter()
         {
-
         }
 
         // public void erstelleExport(String importPfad,String pfadExport, String nameLehrer)
-        public void erstelleExport(String importPfad)
+        public void erstelleExport(string importPfad, string exportPfad, string jahr, string lehrer)
         {
+            WorkBook workbook = WorkBook.Load(importPfad);
+            WorkSheet sheet = workbook.DefaultWorkSheet;
 
-            Excel.Application excel = new Excel.ApplicationClass();
-            excel.Visible = true;
+            sheet["G1"].Value = daten[0] + "." + jahr;
+            sheet["C48"].Value = lehrer;
+            //Datum der Fehltage
+            sheet["D7"].Value = daten[0];
+            sheet["F7"].Value = daten[1];
+            sheet["H7"].Value = daten[2];
+            sheet["J7"].Value = daten[3];
+            sheet["L7"].Value = daten[4];
 
-            Excel.Workbook workbook = excel.Workbooks.Open(importPfad);
-
-            Excel.Worksheet worksheet = (Excel.Worksheet)workbook.Worksheets["Auswertung"];
-            try
+            for (int i = 9; i - 9 < schuelerListe.Length; i++)
             {
-                for (int i = 9; i <= schuelerListe.Length; i++)
-                {
-                    //Index, Name, Vorname
-                    worksheet.Cells[i, 0] = i - 9;
-                    worksheet.Cells[i, 1] = schuelerListe[i].Nachname;
-                    worksheet.Cells[i, 2] = schuelerListe[i].Vorname;
 
-                    //Fehlzeiten
-                    for (int j = 0; j <= schuelerListe[i].Fehlzeit.Length; j++)
+                sheet["A" + i].Value = i - 9;
+                sheet["B" + i].Value = schuelerListe[i - 9].Nachname;
+                sheet["C" + i].Value = schuelerListe[i - 9].Vorname;
+
+
+                //Fehlzeiten
+                for (int j = 0; j < schuelerListe[i - 9].Fehlzeit.Length; j++)
+                {
+                    if (schuelerListe[i - 9].Fehlzeit[j] != null)
                     {
-                        worksheet.Cells[i, j + 3] = schuelerListe[i].Fehlzeit[j].Stunden;
-                        worksheet.Cells[i, j + 4] = schuelerListe[i].Fehlzeit[j].Status;
-                    }
-                    //Summe
-                    worksheet.Cells[i, 2] = schuelerListe[i].Vorname;
-                    for (int j = 14; j <= 16; j++)
-                    {
-                        worksheet.Cells[i, j] = fehlzeitenSummenListe[i, j - 14];
+                        switch (j)
+                        {
+                            case 0:
+                                sheet["D" + i].Value = schuelerListe[i - 9].Fehlzeit[j].Stunden;
+                                sheet["E" + i].Value = schuelerListe[i - 9].Fehlzeit[j].Status;
+                                break;
+                            case 1:
+                                sheet["F" + i].Value = schuelerListe[i - 9].Fehlzeit[j].Stunden;
+                                sheet["G" + i].Value = schuelerListe[i - 9].Fehlzeit[j].Status;
+                                break;
+                            case 2:
+                                sheet["H" + i].Value = schuelerListe[i - 9].Fehlzeit[j].Stunden;
+                                sheet["I" + i].Value = schuelerListe[i - 9].Fehlzeit[j].Status;
+                                break;
+                            case 3:
+                                sheet["J" + i].Value = schuelerListe[i - 9].Fehlzeit[j].Stunden;
+                                sheet["K" + i].Value = schuelerListe[i - 9].Fehlzeit[j].Status;
+                                break;
+                            case 4:
+                                sheet["L" + i].Value = schuelerListe[i - 9].Fehlzeit[j].Stunden;
+                                sheet["M" + i].Value = schuelerListe[i - 9].Fehlzeit[j].Status;
+                                break;
+                        }
                     }
                 }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                Console.WriteLine("Es gab ein Problem bei der Beschreibung von Excel");
+                //Summe
+                //erstelleAuswertung();
+                sheet["N" + i].Value = schuelerListe[i - 9].Verspaetet; //Verspätung
+                sheet["O" + i].Value = schuelerListe[i - 9].Entschuldigt; //Entschuldigt
+                sheet["P" + i].Value = schuelerListe[i - 9].Offen; //offen
+
             }
 
             // Arbeitsmappe speichern 
-            workbook.Save();
-
-            // Excel beenden
-            excel.Quit();
+            workbook.SaveAs(exportPfad);
         }
-
-        public void erstelleAuswertung()
-        {
-            //Summe der Fehlstunden
-            int[,] summeFehlstunden = new int[schuelerListe.Length, 4];
-
-            for (int i = 0; i <= schuelerListe.Length; i++)
-            {
-                for (int j = 0; j <= schuelerListe[i].Fehlzeit.Length; j++)
-                {
-                    if (schuelerListe[i].Fehlzeit[j].Status.Equals("offen"))
-                        //unendschuldigt
-                        summeFehlstunden[i, 1] += schuelerListe[i].Fehlzeit[j].Stunden;
-                    else if (schuelerListe[i].Fehlzeit[j].Status.Equals("verspätet"))
-                        //Verspätet
-                        summeFehlstunden[i, 2] += schuelerListe[i].Fehlzeit[j].Stunden;
-                    else
-                        //Entschuldigt
-                        summeFehlstunden[i, 3] += schuelerListe[i].Fehlzeit[j].Stunden;
-                }
-            }
-            this.fehlzeitenSummenListe = summeFehlstunden;
-        }
-
         public Klasse importReport(String pfadImport)
         {
             //CSV-Pfad
@@ -95,7 +91,7 @@ namespace WU_Aufbereitung.models
             List<string> listA = new List<string>();
 
             int[] zeiten = new int[5];
-            int hilfe = 0;
+            int zeileCSV = 0;
             String[] tage = new String[5];
             String[] zeile;
             List<Schueler> schueler = new List<Schueler>();
@@ -104,6 +100,7 @@ namespace WU_Aufbereitung.models
             {
                 var line = reader.ReadLine();
                 var values = line.Split(';');
+
                 for (int i = 0; i < values.Length; i++)
                 {
                     if (values[i] != "")
@@ -112,51 +109,94 @@ namespace WU_Aufbereitung.models
                     }
                 }
 
-                if (hilfe == 0)
+                if (zeileCSV == 0)
                 {
                     tage = listA.ToArray();
+                    daten = listA.ToArray();
                     listA.Clear();
                 }
-                else if (hilfe == 1)
+                else if (zeileCSV == 1)
                 {
-                    hilfe++;
+                    zeileCSV++;
                     listA.Clear();
                     continue;
                 }
-                else if (hilfe >= 2 && !(listA.Count == 0))
+                else if (zeileCSV >= 2 && !(listA.Count == 0))
                 {
 
                     //Zeile mit Inhalt 
                     zeile = listA.ToArray();
 
+                    int tag = 0;
                     //Fehlzeiten 
-                    Fehlzeit[] fehlzeiteSchueler = new Fehlzeit[zeile.Length];
-                    for (int i = 0; i < zeile.Length - 3; i++)
+                    Fehlzeit[] fehlzeiteSchueler = new Fehlzeit[] { null, null, null, null, null };
+                    for (int i = 0; i < 10; i += 2)
                     {
-                        if (!Regex.IsMatch(zeile[i + 3], @"^\d+$"))
+                        if (Regex.IsMatch(values[i + 2], @"^\d+$"))
                         {
-                            fehlzeiteSchueler[i] = new Fehlzeit(tage[i], Int32.Parse(zeile[i + 2]), zeile[i + 3]);
+                            fehlzeiteSchueler[tag] = new Fehlzeit(tage[tag], Int32.Parse(values[i + 2]), values[i + 3]);
+                        }
+                        tag++;
+                    }
+                    //Summe der Fehlzeiten
+                    int verspaetet = 0;
+                    int offen = 0;
+                    int entschuldigt = 0;
+                    for (int i = 0; i < fehlzeiteSchueler.Length; i++)
+                    {
+                        if (fehlzeiteSchueler[i] != null)
+                        {
+                            if (fehlzeiteSchueler[i].Status.Equals("offen"))
+                                //unendschuldigt
+                                offen += fehlzeiteSchueler[i].Stunden;
+                            else if (fehlzeiteSchueler[i].Status.Equals("verspätet"))
+                                //Verspätet
+                                verspaetet += fehlzeiteSchueler[i].Stunden;
+                            else
+                                //Entschuldigt
+                                entschuldigt += fehlzeiteSchueler[i].Stunden;
                         }
                     }
                     //Schüler init
-                    Schueler a = new Schueler(zeile[0], zeile[1], fehlzeiteSchueler);
+                    Schueler a = new Schueler(zeile[0], zeile[1], fehlzeiteSchueler, offen, entschuldigt, verspaetet);
                     schueler.Add(a);
-                    hilfe++;
+                    zeileCSV++;
                     listA.Clear();
                 }
                 else
                 {
                     break;
                 }
-                hilfe++;
+                zeileCSV++;
             }
             SchuelerListe = schueler.ToArray();
-            return new Klasse(schueler, "leer", null);
+            return new Klasse(schueler, "leer");
         }
 
-        public void pruefeReport()
+        public bool pruefeReport(string pfadImport)
         {
+            //Reader Erste Zeile auf Length 5 
+            var reader = new StreamReader(File.OpenRead(pfadImport));
+            List<string> listA = new List<string>();
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(';');
 
+                for (int i = 0; i < values.Length; i++)
+                {
+                    if (values[i] != "")
+                    {
+                        listA.Add(values[i]);
+                    }
+                }
+            }
+            String[] pruefeLaenge = listA.ToArray();
+            if (pruefeLaenge.Length == 5)
+            {
+                return true;
+            }
+            return false;
         }
 
         public void versendeMail(String adresse)
